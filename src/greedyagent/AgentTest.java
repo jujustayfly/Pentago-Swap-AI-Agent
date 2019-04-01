b34da9f1d197e94fb91b8771d30693873de6c8b1
allockicmoi <allockicmoi@gmail.com>
2019-03-31 22:10:21 -0400

    Potential Value graphs implemented
    Residual value graphs are generated for horixontal, vertical,and diagonal value graphs. Still need to research wether to add them or superpose them for best outcome.

--- src/greedyagent/AgentTest.java
+++ src/greedyagent/AgentTest.java
@@ -14,11 +14,44 @@ public class AgentTest {
 	
 	@Test
 	public void TestQuandrantBreakdown() {
+		pbs = new PentagoBoardState();
 		pbs.processMove( new PentagoMove(1,2,Quadrant.TL,Quadrant.BR,0));
 		pbs.processMove( new PentagoMove(4,4,Quadrant.TR,Quadrant.BR,1));
 		pbs.printBoard();
 		BoardAnalyzer analyzer = new BoardAnalyzer(pbs);
 		analyzer.GetQuadrants();
 		analyzer.PrintBoardfromQuadrants();
+	}
+	
+	@Test
+	public void TestPotentialScores() {
+		pbs = new PentagoBoardState();
+		pbs.printBoard();
+		BoardAnalyzer analyzer = new BoardAnalyzer(pbs);
+		analyzer.GetQuadrants();
+		analyzer.AssignMoveValues();
+		System.out.println("Printing horizontal potentials");
+		analyzer.PrintValuesBoardfromQuadrants(analyzer.getHorizontalPotentials());
+		System.out.println("Printing vertical potentials");
+		analyzer.PrintValuesBoardfromQuadrants(analyzer.getVerticalPotentials());
+		System.out.println("Printing diagonal potentials");
+		analyzer.PrintValuesBoardfromQuadrants(analyzer.getDiagonalPotentials());
+	}
+	
+	@Test
+	public void TestPotentialScores2() {
+		pbs = new PentagoBoardState();
+		pbs.processMove((PentagoMove) pbs.getRandomMove());
+		pbs.printBoard();
+		BoardAnalyzer analyzer = new BoardAnalyzer(pbs);
+		analyzer.GetMyColor();
+		analyzer.GetQuadrants();
+		analyzer.AssignMoveValues();
+		System.out.println("Printing horizontal potentials");
+		analyzer.PrintValuesBoardfromQuadrants(analyzer.getHorizontalPotentials());
+		System.out.println("Printing vertical potentials");
+		analyzer.PrintValuesBoardfromQuadrants(analyzer.getVerticalPotentials());
+		System.out.println("Printing diagonal potentials");
+		analyzer.PrintValuesBoardfromQuadrants(analyzer.getDiagonalPotentials());
 	}
 }
--- src/greedyagent/BoardAnalyzer.java
+++ src/greedyagent/BoardAnalyzer.java
@@ -17,16 +17,399 @@ import pentago_swap.PentagoBoardState.Piece;
 public class BoardAnalyzer {
 	private PentagoBoardState pbs;
 	private Piece[][][] quadrants = new Piece[4][3][3];
+	private int[][][] placeValues = new int[4][3][3];
+	private int[][][] verticalPotentials = new int[4][3][3];
+	private int[][][] horizontalPotentials = new int[4][3][3];
+	private int[][][] diagonalPotentials = new int[4][3][3];
+	private Piece mycolor;
+	private Piece opponent;
 
 	public BoardAnalyzer(PentagoBoardState boardState) {
 		this.pbs = boardState;
 	}
 
 	public Move FindBestMove() {
-
+		GetMyColor();
 		GetQuadrants();
+		AssignMoveValues();
 
 		return null;
+
+	}
+
+	public void GetMyColor() {
+		if (pbs.getTurnPlayer() == pbs.WHITE) {
+			mycolor = Piece.WHITE;
+			opponent = Piece.BLACK;
+		}
+		if (pbs.getTurnPlayer() == pbs.BLACK) {
+			mycolor = Piece.BLACK;
+			opponent = Piece.WHITE;
+		}
+
+	}
+
+	void AssignMoveValues() {
+		for (int i = 0; i < 4; i++) {
+			UpdateQuandrantValues(i);
+			}
+		}
+
+	
+
+	
+
+	private void UpdateQuandrantValues(int i) {
+		Piece[][] quadrant = new Piece[3][3];
+		quadrant = quadrants[i];
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				UpdatePotential(i, j, k);
+			}
+		}
+
+	}
+
+	private void UpdatePotential(int i, int j, int k) {
+		if (quadrants[i][j][k] == Piece.WHITE || quadrants[i][j][k] == Piece.BLACK) {
+			placeValues[i][j][k] = Integer.MIN_VALUE + 1;
+		} else {
+			UpdateHorizontalPotential(i, j, k);
+			UpdateVerticalPotential(i, j, k);
+			UpdateDiagonalPotential(i, j, k);
+		}
+
+	}
+
+	private void UpdateDiagonalPotential(int i, int j, int k) {
+		if (k == j && j == 1) {
+			UpdateNegSlopeDiagPotentials(i, j, k);
+			UpdatePosSlopeDiagPotentials(i, j, k);
+
+		} else if (k == j) {
+			UpdateNegSlopeDiagPotentials(i, j, k);
+			UpdateSmallDiagPotentials(i, j, k);
+		} else if (k + j == 2) {
+			UpdatePosSlopeDiagPotentials(i, j, k);
+			UpdateSmallDiagPotentials(i, j, k);
+		} else {
+			UpdateSmallDiagPotentials(i, j, k);
+		}
+
+	}
+
+	private void UpdateNegSlopeDiagPotentials(int i, int j, int k) {
+		if (quadrants[i][1][1] == opponent) {
+			getDiagonalPotentials()[i][j][k] += 0;
+		} else {
+			if (j == 0 && k == 0) {
+				if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][2][2] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][2][2] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][2][2] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][2][2] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][2][2] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 5;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][2][2] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else {
+					System.out.println("Error calculating Neg Slope Potential");
+				}
+			}
+			else if( j ==1 && k==1) {
+				//need 9 here
+				if(quadrants[i][0][0]==Piece.EMPTY && quadrants[i][2][2]==Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				}
+				else if(quadrants[i][0][0]==Piece.EMPTY && quadrants[i][2][2]==mycolor) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				}
+				else if(quadrants[i][0][0]==Piece.EMPTY && quadrants[i][2][2]==opponent) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				}
+				else if(quadrants[i][0][0]==mycolor && quadrants[i][2][2]==Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				}
+				else if(quadrants[i][0][0]==mycolor && quadrants[i][2][2]==mycolor) {
+					getDiagonalPotentials()[i][j][k] += 5;
+				}
+				else if(quadrants[i][0][0]==mycolor && quadrants[i][2][2]==opponent) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				}
+				else if(quadrants[i][0][0]==opponent && quadrants[i][2][2]==Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				}
+				else if(quadrants[i][0][0]==opponent && quadrants[i][2][2]==opponent) {
+					getDiagonalPotentials()[i][j][k] += 0;
+				}
+				else if(quadrants[i][0][0]==opponent && quadrants[i][2][2]==mycolor) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				}else {
+					System.out.println("Error calculating Neg Slope Potential");
+				}
+			}
+			else if (j == 2 && k == 2) {
+				if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][0][0] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][0][0] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][0][0] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][0][0] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][0][0] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 5;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][0][0] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else {
+					System.out.println("Error calculating Neg Slope Potential");
+				}
+			}
+		}
+
+	}
+
+	private void UpdateSmallDiagPotentials(int i, int j, int k) {
+		if(j==0 &&k ==1) {
+			if(quadrants[i][1][0]==Piece.EMPTY && quadrants[i][1][2]==Piece.EMPTY) {
+				getDiagonalPotentials()[i][j][k]+= 2;
+				}
+			else if(!(quadrants[i][1][0]==opponent && quadrants[i][1][2]==opponent)) {
+				getDiagonalPotentials()[i][j][k]+= 1;
+			}
+		}
+		else if(j==2 &&k ==1) {
+			if(quadrants[i][1][0]==Piece.EMPTY && quadrants[i][1][2]==Piece.EMPTY) {
+				getDiagonalPotentials()[i][j][k]+= 2;
+				}
+			else if(!(quadrants[i][1][0]==opponent && quadrants[i][1][2]==opponent)) {
+				getDiagonalPotentials()[i][j][k]+= 1;
+			}
+		}
+		else if(j==1 &&k ==0) {
+			if(quadrants[i][0][1]==Piece.EMPTY && quadrants[i][2][1]==Piece.EMPTY) {
+				getDiagonalPotentials()[i][j][k]+= 2;
+				}
+			else if(!(quadrants[i][0][1]==opponent && quadrants[i][2][1]==opponent)) {
+				getDiagonalPotentials()[i][j][k]+= 1;
+			}
+		}
+		else if(j==1 &&k ==2) {
+			if(quadrants[i][0][1]==Piece.EMPTY && quadrants[i][2][1]==Piece.EMPTY) {
+				getDiagonalPotentials()[i][j][k]+= 2;
+				}
+			else if(!(quadrants[i][0][1]==opponent && quadrants[i][2][1]==opponent)) {
+				getDiagonalPotentials()[i][j][k]+= 1;
+			}
+		}
+
+	}
+
+	private void UpdatePosSlopeDiagPotentials(int i, int j, int k) {
+		if (quadrants[i][1][1] == opponent) {
+			getDiagonalPotentials()[i][j][k] += 0;
+		} else {
+			if (j == 2 && k == 0) {
+				if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][0][2] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][0][2] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][0][2] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][0][2] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][0][2] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 5;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][0][2] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else {
+					System.out.println("Error calculating Pos Slope Potential");
+				}
+			}
+			else if( j ==1 && k==1) {
+				//need 9 here
+				if(quadrants[i][0][2]==Piece.EMPTY && quadrants[i][2][0]==Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				}
+				else if(quadrants[i][0][2]==Piece.EMPTY && quadrants[i][2][0]==mycolor) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				}
+				else if(quadrants[i][0][2]==Piece.EMPTY && quadrants[i][2][0]==opponent) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				}
+				else if(quadrants[i][0][2]==mycolor && quadrants[i][2][0]==Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				}
+				else if(quadrants[i][0][2]==mycolor && quadrants[i][2][0]==mycolor) {
+					getDiagonalPotentials()[i][j][k] += 5;
+				}
+				else if(quadrants[i][0][2]==mycolor && quadrants[i][2][0]==opponent) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				}
+				else if(quadrants[i][0][2]==opponent && quadrants[i][2][0]==Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				}
+				else if(quadrants[i][0][2]==opponent && quadrants[i][2][0]==opponent) {
+					getDiagonalPotentials()[i][j][k] += 0;
+				}
+				else if(quadrants[i][0][2]==opponent && quadrants[i][2][0]==mycolor) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				}
+				else {
+					System.out.println("Error calculating Pos Slope Potential");
+				}
+			}
+			else if (j == 0 && k == 2) {
+				if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][2][0] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][2][0] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == Piece.EMPTY && quadrants[i][2][0] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][2][0] == Piece.EMPTY) {
+					getDiagonalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][2][0] == mycolor) {
+					getDiagonalPotentials()[i][j][k] += 5;
+				} else if (quadrants[i][1][1] == mycolor && quadrants[i][2][0] == opponent) {
+					getDiagonalPotentials()[i][j][k] += 3;
+				} else {
+					System.out.println("Error calculating Pos Slope Potential");
+				}
+			}
+		}
+
+	}
+
+	
+
+	private void UpdateVerticalPotential(int i, int j, int k) {
+		if (quadrants[i][1][k] == opponent) {
+			getVerticalPotentials()[i][j][k] += 0;
+		} else {
+			if (j == 0) {
+				if (quadrants[i][1][k] == Piece.EMPTY && quadrants[i][2][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][k] == Piece.EMPTY && quadrants[i][2][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][k] == Piece.EMPTY && quadrants[i][2][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][1][k] == mycolor && quadrants[i][2][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][k] == mycolor && quadrants[i][2][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 6;
+				} else if (quadrants[i][1][k] == mycolor && quadrants[i][2][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 5;
+				} else {
+					System.out.println("Error calculating vertical potential");
+				}
+			} else if (j == 1) {
+				if (quadrants[i][0][k] == Piece.EMPTY && quadrants[i][2][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][0][k] == Piece.EMPTY && quadrants[i][2][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][0][k] == mycolor && quadrants[i][2][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][0][k] == Piece.EMPTY && quadrants[i][2][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][0][k] == mycolor && quadrants[i][2][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 6;
+				} else if (quadrants[i][0][k] == mycolor && quadrants[i][2][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][0][k] == opponent && quadrants[i][2][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][0][k] == opponent && quadrants[i][2][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][0][k] == opponent && quadrants[i][2][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 0;
+				} else {
+					System.out.println("Error calculating vertical potential");
+				}
+			} else {
+				if (quadrants[i][1][k] == Piece.EMPTY && quadrants[i][0][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][k] == Piece.EMPTY && quadrants[i][0][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][1][k] == Piece.EMPTY && quadrants[i][0][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][1][k] == mycolor && quadrants[i][0][k] == opponent) {
+					getVerticalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][1][k] == mycolor && quadrants[i][0][k] == mycolor) {
+					getVerticalPotentials()[i][j][k] += 6;
+				} else if (quadrants[i][1][k] == mycolor && quadrants[i][0][k] == Piece.EMPTY) {
+					getVerticalPotentials()[i][j][k] += 5;
+				} else {
+					System.out.println("Error calculating vertical potential");
+				}
+			}
+
+		}
+
+	}
+
+	private void UpdateHorizontalPotential(int i, int j, int k) {
+		if (quadrants[i][j][1] == opponent) {
+			getHorizontalPotentials()[i][j][k] += 0;
+		} else {
+			if (k == 0) {
+				if (quadrants[i][j][1] == Piece.EMPTY && quadrants[i][j][2] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][1] == Piece.EMPTY && quadrants[i][j][2] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][j][1] == Piece.EMPTY && quadrants[i][j][2] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][j][1] == mycolor && quadrants[i][j][2] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][1] == mycolor && quadrants[i][j][2] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 6;
+				} else if (quadrants[i][2][1] == mycolor && quadrants[i][j][2] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 5;
+				} else {
+					System.out.println("Error calculating vertical potential");
+				}
+			} else if (k == 1) {
+				if (quadrants[i][j][0] == Piece.EMPTY && quadrants[i][j][2] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][0] == Piece.EMPTY && quadrants[i][j][2] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][j][0] == mycolor && quadrants[i][j][2] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][j][0] == Piece.EMPTY && quadrants[i][j][2] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][j][0] == mycolor && quadrants[i][j][2] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 6;
+				} else if (quadrants[i][j][0] == mycolor && quadrants[i][j][2] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][0] == opponent && quadrants[i][j][2] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][0] == opponent && quadrants[i][j][2] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][j][0] == opponent && quadrants[i][j][2] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 0;
+				} else {
+					System.out.println("Error calculating vertical potential");
+				}
+			} else {
+				if (quadrants[i][j][1] == Piece.EMPTY && quadrants[i][j][0] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][1] == Piece.EMPTY && quadrants[i][j][0] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 4;
+				} else if (quadrants[i][j][1] == Piece.EMPTY && quadrants[i][j][0] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 2;
+				} else if (quadrants[i][j][1] == mycolor && quadrants[i][j][0] == opponent) {
+					getHorizontalPotentials()[i][j][k] += 3;
+				} else if (quadrants[i][j][1] == mycolor && quadrants[i][j][0] == mycolor) {
+					getHorizontalPotentials()[i][j][k] += 6;
+				} else if (quadrants[i][j][1] == mycolor && quadrants[i][j][0] == Piece.EMPTY) {
+					getHorizontalPotentials()[i][j][k] += 5;
+				} else {
+					System.out.println("Error calculating vertical potential");
+				}
+			}
+
+		}
 
 	}
 
@@ -53,22 +436,66 @@ public class BoardAnalyzer {
 	void PrintBoardfromQuadrants() {
 		for (int line = 0; line < 3; line++) {
 			for (int i = 0; i < 3; i++) {
-				System.out.print("|"+quadrants[0][line][i].toString());
+				System.out.print("|" + quadrants[0][line][i].toString());
 			}
 			for (int i = 0; i < 3; i++) {
-				System.out.print("|"+quadrants[1][line][i].toString());
+				System.out.print("|" + quadrants[1][line][i].toString());
 			}
 			System.out.print("|\n");
 		}
 		for (int line = 0; line < 3; line++) {
 			for (int i = 0; i < 3; i++) {
-				System.out.print("|"+quadrants[2][line][i].toString());
+				System.out.print("|" + quadrants[2][line][i].toString());
 			}
 			for (int i = 0; i < 3; i++) {
-				System.out.print("|"+quadrants[3][line][i].toString());
+				System.out.print("|" + quadrants[3][line][i].toString());
 			}
 			System.out.print("|\n");
 		}
 	}
 
+	void PrintValuesBoardfromQuadrants(int[][][] quads) {
+		for (int line = 0; line < 3; line++) {
+			for (int i = 0; i < 3; i++) {
+				System.out.print("|" + quads[0][line][i]);
+			}
+			for (int i = 0; i < 3; i++) {
+				System.out.print("|" + quads[1][line][i]);
+			}
+			System.out.print("|\n");
+		}
+		for (int line = 0; line < 3; line++) {
+			for (int i = 0; i < 3; i++) {
+				System.out.print("|" + quads[2][line][i]);
+			}
+			for (int i = 0; i < 3; i++) {
+				System.out.print("|" + quads[3][line][i]);
+			}
+			System.out.print("|\n");
+		}
+	}
+
+	public int[][][] getVerticalPotentials() {
+		return verticalPotentials;
+	}
+
+	public void setVerticalPotentials(int[][][] verticalPotentials) {
+		this.verticalPotentials = verticalPotentials;
+	}
+
+	public int[][][] getHorizontalPotentials() {
+		return horizontalPotentials;
+	}
+
+	public void setHorizontalPotentials(int[][][] horizontalPotentials) {
+		this.horizontalPotentials = horizontalPotentials;
+	}
+
+	public int[][][] getDiagonalPotentials() {
+		return diagonalPotentials;
+	}
+
+	public void setDiagonalPotentials(int[][][] diagonalPotentials) {
+		this.diagonalPotentials = diagonalPotentials;
+	}
 }
